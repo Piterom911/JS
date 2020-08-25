c994d258026e9c4385a371f6343d1801e483b6e3
Roman <piterom911@gmail.com>
2020-08-25 13:49:14 +0600

    first code — 'movies'

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
-let hello = () => {
-    "use strict";
+"use strict";
 
-let arr = ['Me', 'My Friend'];
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
 
-console.log(arr[1]);
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
+
+let movieOne = prompt("Один из последних просмотренных фильмов?","");
+let ratingOne = prompt("На сколько оцените его?","");
+let movieTwo = prompt("Один из последних просмотренных фильмов?","");
+let ratingTwo = prompt("На сколько оцените его?","");
+
+personalMovieDB.movies[movieOne] = ratingOne;
+personalMovieDB.movies[movieTwo] = ratingTwo;
+
+console.log(personalMovieDB);
